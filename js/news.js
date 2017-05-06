4e5bfa5c6bd26da24027103739cca8e61384873c
am4rtinez <anglxd@gmail.com>
2017-05-06 13:01:23 +0200

    Cambios en estilo banner
    Realizado el estilo para el banner y los distintos dispositivos.
    En disp moviles <768px aparece en el top y al hacer scroll se oculta. Al
    volver al top de la ventana se muestra con un height de 90px.
    En dispositivos menores a 992px se muestra el banner con 200px de
    anchura.
    Resto de dispositivos se muestra con 300px de anchura.

--- js/news.js
+++ js/news.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
-    
-	$(window).scroll(function(){
-		$(".advert-container").hide();
-	});
+
+	/*
+	 * Comprueba si la ventana modifica su tamaño.
+	 * En el caso correcto si el tamñano es menor a 768 píxels se comprueba si esta en el top 
+	 * para activar el banner en dispositivos móviles. En caso contrario se deja el banner activo.
+	*/
+	window.onresize = function() {
+    	$(window).scroll(function(){
+    		if ($(window).width() < 768) {
+    			if ($(window).scrollTop() == 0){
+					$(".advert-container").show();
+				} else  {
+					$(".advert-container").hide();
+				}
+    		} else {
+    			$(".advert-container").show();
+    		}			
+		});	
+	}
 
 });
