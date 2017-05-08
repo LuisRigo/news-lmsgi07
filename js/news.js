1642c5075bd6ce1282802ce4b49717fe61c67242
am4rtinez <anglxd@gmail.com>
2017-05-08 20:38:51 +0200

    Modificaciones JS para carga de noticias
    Implementada la carga de noticias con scrolling y botón de mas noticias.

--- js/news.js
+++ js/news.js
@@ -41,29 +41,76 @@ $(document).ready(function(){
 		} else if (i == 1){
 			cargarJSON(i+1);
 			i++;
-		} else {
-			//Do Nothing;
-		}
+			$("#more-news").hide();
+		} 
 	});
 
-	if (($(window).scroll) && (scroll_bool)){
+	$(window).scroll(function(){
+		if (($(window).scrollBottom()==0) && (scroll_bool)){
+			if (i == 0){
+				cargarJSON(i+1);
+				i++;
+			} else if (i == 1){
+				cargarJSON(i+1);
+				i++;
+				$("#more-news").hide();
+			}
+		};		
+	});
 
+	$.fn.scrollBottom = function() { 
+		return $(document).height() - this.scrollTop() - this.height(); 
 	};
 
 	function cargarJSON(i){
 		fichero = "data/" + i + ".json";
-		$.getJSON( fichero, function( jsonObject ) {
-	        ponerComunidades( jsonObject );
+		$.getJSON(fichero, function(jsonObject) {
+	        ponerNoticias(jsonObject);
 	    });
 	}
 
-	function ponerComunidades(json){
-     $.each( json, function( j, item ) {
-     	console.log(item.id);
-         //dos formas de hacer lo mismo, la segunda es más adecuada.
-         //$("#comunidades").append( "<option value='" + comunidad.slug + "'>" + comunidad.comunidad + "</option>" );
-         //$('.main-container').append($('<option>', { value: comunidad.slug, text : comunidad.comunidad }));
-         //"slug" es la parte de la url, por si hay que visitar la página de la comunidad (no va con vuestro proyecto, es un ejemplo)
+	function ponerNoticias(json){
+     $.each( json, function(j, item) {
+
+		var noticia_container = $('<div>');
+		var img = $('<img>');
+		var section = $('<section>');
+		var h2  = $('<h2>');
+		var datetime = $('<div>');
+		var info = $('<div>');
+		var p   = $('<p>');
+		var imagen = $('<div>');
+
+		var li = $('<li>');
+     	var a = $('<a>');
+
+		noticia_container.attr('id', item.id);
+		noticia_container.attr('class', 'container-fluid');
+		section.attr('class', 'noticias');
+		h2.text(item.titulo);
+		datetime.attr('class', 'datetime');
+		datetime.text(item.datetime);
+		info.attr('class', 'informacion');
+		p.text(item.desc);
+		datetime.attr('class', 'datetime');
+		datetime.text(item.datetime);
+		img.attr('class', 'img-responsive center-block');
+		img.attr('src', item.imgmid);
+
+		h2.appendTo(section);
+		datetime.appendTo(section);
+		info.appendTo(section);
+		p.appendTo(info);
+		img.appendTo(imagen);
+		imagen.appendTo(section);
+		section.appendTo(noticia_container);
+     	noticia_container.appendTo('.main-container');
+
+     	a.attr('href', '#'+item.id);
+     	a.text(item.nav_element);
+     	a.appendTo(li);
+     	li.appendTo(".nav");
+
      }); 
 }
 
